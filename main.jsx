75770b51592200960b5b3382f445dc0bc1480cf3
John Matthews <johnmatthews2634@gmail.com>
2018-08-22 13:08:54 -0700

    add stylesheets folder and setup root on entry file

--- main.jsx
+++ main.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+class Root extends React.Component {
+  render() {
+    return(
+      <div>
+        John
+      </div>
+    );
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>John</h1>, root);
+  ReactDOM.render(<Root/>, root);
 });
