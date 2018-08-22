1a1b9a5c9769b903b47844d79a170bdc70d09be9
John Matthews <johnmatthews2634@gmail.com>
2018-08-22 13:04:33 -0700

    set up entry file and webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var webpack = require("webpack");
+
+
+module.exports = {
+  context: __dirname,
+  entry: "./main.jsx",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
